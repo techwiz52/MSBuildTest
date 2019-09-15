96939d61208d6d3afd3f3746ee183a1a84c728b6
techwiz85 <54539385+techwiz52@users.noreply.github.com>
2019-09-15 17:56:46 -0400

    break the build.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //int89 i = 0; // error
+            int89 i = 0; // break the build
             // Make a change 19
         }
     }
